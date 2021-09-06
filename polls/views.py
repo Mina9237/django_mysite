94ea82e54e408d98b5364472c1c5c9017cfc6c62
Mina9237 <meinai929@gmail.com>
2021-09-07 05:09:55 +0900

    初めてのアプリ作成その３完成

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,5 @@
+import polls
+from django.http.response import Http404
 from django.shortcuts import render
 
 # Create your views here.
@@ -8,8 +10,8 @@ from .models import Question
 #def index(request):
     #return HttpResponse("Hello,world. You're at the polls index.")
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s.") % question_id
+#def detail(request, question_id):
+    #return HttpResponse("You're looking at question %s.") % question_id
 
 def results(request,question_id):
     response = "You're looking at the result of question %s"
@@ -36,3 +38,10 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
