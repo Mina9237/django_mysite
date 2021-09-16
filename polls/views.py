dab2c8ce601599dae659bd331687350ca86f2ef3
Mina9237 <meinai929@gmail.com>
2021-09-17 05:27:00 +0900

    CSSの適用

--- polls/views.py
+++ polls/views.py
@@ -91,20 +91,5 @@ class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
 
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
 
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.filter(
-            pub_date_lte = timezone.now()
-        ).order_by('-pub_date')[:5]
 
-class DetailView(generic.DetailView):
-    ...
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
